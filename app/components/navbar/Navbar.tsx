9cb31ad62b725c6f6a1423459e3ecac9034e2c69
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2023-09-13 09:52:51 -0400

    Add navbar component

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { Alkatra } from "next/font/google";
+
+const alkatra = Alkatra({
+  subsets: ["latin"],
+});
+
+const Navbar = () => {
+  return (
+    <div className="relative h-[75px] w-full hidden md:flex justify-between items-center px-8 bg-transparent text-ctp-text top-0 z-[2]">
+      <div className="font-bold flex justify-center text-2xl">
+        <div className={alkatra.className}>
+          <Link href="/" className="text-ctp-text hover:text-white">
+            Kyle Curtis
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Link
+          href="/"
+          scroll={false}
+          className="text-ctp-text hover:text-[white] ml-5"
+        >
+          Education
+        </Link>
+        <Link
+          href="/"
+          scroll={false}
+          className="text-ctp-text hover:text-[white] ml-5"
+        >
+          Certifications
+        </Link>
+        <Link
+          href="/"
+          scroll={false}
+          className="text-ctp-text hover:text-[white] ml-5"
+        >
+          Stack
+        </Link>
+        <Link
+          href="/"
+          scroll={false}
+          className="text-ctp-text hover:text-[white] ml-5"
+        >
+          Experience
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <h1 className="text-ctp-text">Kyle Curtis</h1>
     </main>
   )
